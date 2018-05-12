f44200ae6ac5ec9638b3641f8b8d2869c496f386
Patrick Gao <patrickgao0922@gmail.com>
2018-05-12 11:21:26 +1000

    initial commit

---
+++ VistusaPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VistusaPractice
+//
+//  Created by Patrick Gao on 11/5/18.
+//  Copyright © 2018 Patrick Gao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
