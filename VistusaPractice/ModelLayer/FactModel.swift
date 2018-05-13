76f2f0be611660da2b2fc4c79e7ebb966e55ad08
Patrick Gao <patrickgao0922@gmail.com>
2018-05-13 13:26:15 +1000

    create model layer

---
+++ VistusaPractice/ModelLayer/FactModel.swift
@@ -0,0 +1,16 @@
+//
+//  FactModel.swift
+//  VistusaPractice
+//
+//  Created by Patrick Gao on 13/5/18.
+//  Copyright © 2018 Patrick Gao. All rights reserved.
+//
+
+import Foundation
+protocol FactModel {
+    
+}
+
+class FactModelImplementation:FactModel {
+    
+}
