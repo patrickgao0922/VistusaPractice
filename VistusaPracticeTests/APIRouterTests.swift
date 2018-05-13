878f5c9c16d0c5e4ec17a6773768e133df205cae
Patrick Gao <patrickgao0922@gmail.com>
2018-05-13 10:55:31 +1000

    finish router test

--- VistusaPracticeTests/APIRouterTests.swift
+++ VistusaPracticeTests/APIRouterTests.swift
@@ -9,13 +9,15 @@
 import Foundation
 import Quick
 import Nimble
+import Alamofire
 @testable import VistusaPractice
 
 class APIRouterTests:QuickSpec {
     override func spec() {
         it("Test Route") {
             let request = APIRouter.fact.urlRequest
-            
+            expect(request?.url?.absoluteString).to(equal("https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json"))
+            expect(request?.httpMethod).to(equal("GET"))
         }
     }
 }
