b5a2f728fd2ea96ead4ab7b0d80cdf0a81473c0f
Patrick Gao <patrickgao0922@gmail.com>
2018-05-14 15:46:52 +1000

    commit

--- VistusaPractice/View/FactCollectionViewCell.swift
+++ VistusaPractice/View/FactCollectionViewCell.swift
@@ -13,20 +13,34 @@ class FactCollectionViewCell: UICollectionViewCell {
 
     @IBOutlet var imageView: UIImageView!
     
+    fileprivate var imageDownloadingSub:Disposable?
+    
     fileprivate weak var viewModel:FactCollectionViewCellViewModel? = nil
+    fileprivate var disposeBag:DisposeBag = DisposeBag()
     
     func config(with rowDTO:RowDTO, using viewModel:FactCollectionViewCellViewModel? = nil) {
         if let viewModel = viewModel {
             self.viewModel = viewModel
         }
+        self.imageView.image = nil
+        imageDownloadingSub?.dispose()
+        setupObservables()
+        startDownloadImage()
         
 //        self.titleLabel.text = viewModel?.title
 //        self.descriptionLabel.text = viewModel?.description
+    }
+    
+    func startDownloadImage(){
+        viewModel?.downloadImage()
     }
 }
 
 extension FactCollectionViewCell {
     func setupObservables() {
-        
+        imageDownloadingSub = viewModel?.image.asDriver().asObservable().skip(1).subscribe(onNext: { (image) in
+            self.imageView.image = image
+        })
+        imageDownloadingSub?.disposed(by: disposeBag)
     }
 }
