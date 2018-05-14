b23829946ecf25075d3d734afea2c8f208d6d07d
Patrick Gao <patrickgao0922@gmail.com>
2018-05-14 12:11:10 +1000

    tested fetchFact in view model

--- VistusaPractice/Model View/FactCollectionViewViewModel.swift
+++ VistusaPractice/Model View/FactCollectionViewViewModel.swift
@@ -7,19 +7,36 @@
 //
 
 import Foundation
+import RxSwift
+import RxCocoa
 
-protocol FactCollectionViewViewModel {}
+protocol FactCollectionViewViewModel {
+    var fact:Variable<FactDTO?> {get}
+    func fetchFact()
+}
 
 class FactCollectionViewViewModelImplementation:FactCollectionViewViewModel {
     
     fileprivate var modelLayer:FactModelLayer
+    fileprivate var disposeBag:DisposeBag
+    
+//    var cellViewModels:
+    
+    var fact:Variable<FactDTO?>
     init (withFactModelLayer modelLayer:FactModelLayer) {
         self.modelLayer = modelLayer
+        disposeBag = DisposeBag()
+        fact = Variable<FactDTO?>(nil)
     }
     
     func fetchFact() {
-        
+        modelLayer.fetchFact().subscribe { (single) in
+            switch single {
+            case .success(let factDTO):
+                self.fact.value = factDTO
+            case .error(_):
+                break
+            }
+        }.disposed(by: disposeBag)
     }
 }
-
-//struct FactSection
--- VistusaPracticeTests/FactCollectionViewViewModelTests.swift
+++ VistusaPracticeTests/FactCollectionViewViewModelTests.swift
@@ -10,6 +10,7 @@ import Foundation
 import Quick
 import Nimble
 import RxBlocking
+import RxSwift
 import Swinject
 @testable import VistusaPractice
 
@@ -17,9 +18,18 @@ class FactCollectionViewViewModelTests:QuickSpec {
     override func spec() {
         let container: Container = Container()
         let dependencyRegistry:DependencyRegistry = DependencyRegistry(withContainer: container)
-        let viewModel = dependencyRegistry.container.resolve(FactCollectionViewViewModel.self)
+        let viewModel = dependencyRegistry.container.resolve(FactCollectionViewViewModel.self)!
         
         it("Test fetch Fact") {
+            
+            waitUntil(timeout: 5.0, action: { (done) in
+                _ = viewModel.fact.asObservable().skip(1).subscribe(onNext: { (factDTO) in
+                    expect(factDTO).notTo(beNil())
+                    expect(factDTO!.title).to(equal("About Canada"))
+                    done()
+                })
+                viewModel.fetchFact()
+            })
             
         }
     }
